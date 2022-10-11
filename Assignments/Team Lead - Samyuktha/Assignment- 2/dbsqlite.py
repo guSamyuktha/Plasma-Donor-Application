d8ec88bcae46099a1fe8622b4f94e7c83cbce9d5
guSamyuktha <103623425+guSamyuktha@users.noreply.github.com>
2022-10-11 11:57:07 +0530

    Update dbsqlite.py

--- Assignments/Team Lead - Samyuktha/Assignment- 2/dbsqlite.py
+++ Assignments/Team Lead - Samyuktha/Assignment- 2/dbsqlite.py
@@ -1,8 +1,8 @@
 import sqlite3
 
-conn = sqlite3.connect('student_database.db')
+conn = sqlite3.connect('register.db')
 print("Opened database successfully")
 
-conn.execute('CREATE TABLE students (name TEXT, addr TEXT, city TEXT, pin TEXT)')
+conn.execute('CREATE TABLE students (username TEXT, email TEXT, password TEXT)')
 print("Table created successfully")
 conn.close()
